f08bf6d904ecc050d55b14493668fd2f9befbeb8
thomue100 <46538287+thomue100@users.noreply.github.com>
2023-06-03 17:25:30 +0200

    Merge remote-tracking branch 'Routenplaner/master'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,26 @@
 import unittest
 from geopy.distance import geodesic
-from main import berechne_gesamtstrecke, stadt_koordinaten
+from main import berechne_gesamtstrecke, berechne_kompakte_route, stadt_koordinaten
 
+class TestBerechneKompakteRoute(unittest.TestCase):
+    def test_berechne_kompakte_route(self):
+        selected_cities = ["Berlin", "Hamburg", "München", "Düsseldorf", "Bremen"]
+        tour = berechne_kompakte_route(selected_cities)
+        self.assertEqual(tour[0], "Berlin")
+        self.assertEqual(tour[-1], "Berlin")
+        self.assertEqual(set(tour), set(selected_cities))
+
+    def test_berechne_kompakte_route_kuerzeste_route(self):
+        selected_cities = ["Berlin", "Hamburg", "München", "Düsseldorf", "Bremen"]
+        tour = berechne_kompakte_route(selected_cities)
+        tour_length = 0
+        for i in range(len(tour) - 1):
+            tour_length += geodesic(
+                stadt_koordinaten[tour[i]], stadt_koordinaten[tour[i + 1]]
+            ).kilometers
+        #Der Algorithmus berechnet eine kurze, aber nicht unbedingt die kürzeste
+        #Distanz. Wir akzeptieren alles, was 1550 bis 1650.
+        self.assertAlmostEqual(tour_length, 1600, delta=50)
 class TestBerechneGesamtstrecke(unittest.TestCase):
 
     def test_berechne_gesamtstrecke(self):
